46b23d118576b7592c2bea313bf088a7046d24d2
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-06-20 11:49:43 +0200

    removed hero from login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./../assets/styles/styles.css"
 import Header from '../components/Header'
-import Hero from '../components/Hero'
 import LoginForm from '../components/LoginForm'
 import Footer from '../components/Footer'
 
@@ -10,7 +9,6 @@ const Login: React.FC = () => {
     return (
         <>
             <Header />
-            <Hero />
             <LoginForm />
             <Footer />
         </>
